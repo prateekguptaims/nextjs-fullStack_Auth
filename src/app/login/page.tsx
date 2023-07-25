b4128a7e641a019b5161b6b50b3d569b83888791
prateekguptaims <prateekkummargupta45@gmail.com>
2023-07-25 22:36:39 +0530

    change2

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/router';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/router';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 
